70754716bd6404ac29e0595dfd2590ba5071e257
an244 <thienan.le244@gmail.com>
2017-10-20 12:30:48 +0700

    ngFor don gian

--- src/app/struct/struct.component.ts
+++ src/app/struct/struct.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class StructComponent implements OnInit {
 
   isShow = false;
+  arrSubjects = ['Angular', 'NodeJS', 'React'];
 
   constructor() { }
 
