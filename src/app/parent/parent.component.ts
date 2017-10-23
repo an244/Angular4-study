2f0227d007328a031cb3a860262e82dab7979463
an244 <thienan.le244@gmail.com>
2017-10-23 15:02:01 +0700

    su dung template variable va ViewChild

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+// B1 VC: import thu vien ViewChild va ChildComponent vo.
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChildComponent } from '../child/child.component';
 
 @Component({
   selector: 'app-parent',
@@ -6,16 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./parent.component.css']
 })
 export class ParentComponent implements OnInit {
-  
+  // B2 VC: export ChildComponent
+  @ViewChild(ChildComponent)
+  // B3 VC: khai bao 1 bien kieu ChildComponent de truy cap
+  myChildComponent: ChildComponent;
+  // B4 VC: tao 1 function de add
+  onAddForChild() {
+    this.myChildComponent.valueChild++;
+  }
   valueParent = 0;
   constructor() { }
   ngOnInit() {
   }
 
-  //Buoc 3: khai bao func changeValue() da dc su dung o buoc 1.
+  // Buoc 3: khai bao func changeValue() da dc su dung o buoc 1.
   changeValue(isAdd: boolean) {
-    if(isAdd=== true) {this.valueParent += 1};
-    if(isAdd=== false) {this.valueParent -= 1};
+    if (isAdd === true) {this.valueParent += 1; }
+    if (isAdd === false) {this.valueParent -= 1; }
    
   }
 }
